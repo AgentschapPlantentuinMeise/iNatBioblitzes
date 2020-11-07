63e8ac8248fdbf8d4b3ad025934615af7ffcc222
Quentin Groom <quentin.groom@plantentuinmeise.be>
2020-11-07 14:25:42 +0100

    Adding R commands used to plot graphs

---
+++ bioblitzgraphs.R
@@ -0,0 +1,81 @@
+library("tidyverse")
+
+iNatProject <- read.csv("~/notebooks/iNatBioblitzes/iNatProject.csv", encoding="UTF-8", comment.char="#")
+
+#Get rid of projects with 1 or zero observers
+
+projects <- filter(iNatProject, observers > 1)
+projects <- filter(projects, identifiers > 0)
+
+ggplot(data=projects) +
+  geom_point(mapping = aes(x=log(observers),y=log(observations)))
+             
+
+ggplot(data=projects) +
+  geom_point(mapping = aes(x=log(hours/observers),y=log(observations)))
+
+
+ggplot(data=projects) +
+  geom_point(mapping = aes(x=log(observations),y=log(species)))
+
+
+#Species found verses effort
+ggplot(data=projects) +
+  geom_point(mapping = aes(x=hours*observers/1000,y=species, color=project_type)) +
+  scale_x_continuous(trans='log10') +
+  scale_y_continuous(trans='log10') +
+  theme_classic()
+
+ggplot(projects, aes(hours*observers/1000, species)) + geom_point() + geom_smooth(method = "lm", se=FALSE) +
+  scale_x_continuous(trans='log10') +
+  scale_y_continuous(trans='log10') +
+  xlab('Observer effort (length of project (hours) x number of observers)') +
+  ylab('Number of species observed') +
+  theme_classic()
+
+
+ggplot(data=projects) +
+  geom_point(mapping = aes(x=observers,y=identifiers, color=project_type)) + geom_smooth(method = "lm") +
+  scale_x_continuous(trans='log10') +
+  scale_y_continuous(trans='log10') 
+
+
+ggplot(projects, aes(observers, identifiers)) + geom_point() + geom_smooth(method = "lm", se=FALSE) +
+  scale_x_continuous(trans='log10') +
+  scale_y_continuous(trans='log10') 
+
+
+ggplot(projects, aes(hours, observers)) + geom_point() + geom_smooth(method = "lm", se=TRUE) +
+  scale_x_continuous(trans='log10') +
+  scale_y_continuous(trans='log10') +
+  theme_classic()
+
+#What is the average number of observations per bioblitz
+projects %>% summarise(mean_size = mean(observations, na.rm = TRUE))
+
+#What is the average number of observers per bioblitz
+projects %>% summarise(mean_size = mean(observers, na.rm = TRUE))
+
+#What is the average number of species recorded per bioblitz
+projects %>% summarise(mean_size = mean(species, na.rm = TRUE))
+
+#What is the average number of identifiers involved in the bioblitz
+projects %>% summarise(mean_size = mean(identifiers, na.rm = TRUE))
+
+#what is the average length of the bioblitzes in our sample
+projects %>% summarise(mean_size = mean(hours, na.rm = TRUE))
+
+
+
+Mode <- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
+
+
+#The most common length of a bioblitz
+Mode(projects$hours)
+
+
+# A count of all the projects used
+tally(projects)
